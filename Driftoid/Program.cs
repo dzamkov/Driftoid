c24bfd9d67f87b50751c56b58e2cb070f35a26e3
DmitryZamkov <dmitry94@gmail.com>
2010-12-27 17:02:55 -0500

    Started project

---
+++ Driftoid/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Driftoid
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Program main entry point.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            new Window().Run();
+        }
+    }
+}
---
+++ Driftoid/Window.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Driftoid
+{
+    /// <summary>
+    /// Main game window.
+    /// </summary>
+    public class Window : GameWindow
+    {
+        public Window() : base(640, 480, GraphicsMode.Default, "Driftoid")
+        {
+
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            double updatetime = e.Time;
+        }
+    }
+}
